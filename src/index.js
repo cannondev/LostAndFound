84d46af8905ca90e688c0ffc49ba1b8e2dd0dcb3
Cinay Dilibal <71218629+Cdilibal@users.noreply.github.com>
2025-01-22 18:14:57 -0500

    Latest Updates SA2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+const Counter = () => {
+  num++;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+};
+
+setInterval(Counter, 1000);
