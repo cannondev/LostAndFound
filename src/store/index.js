4994bd8a3d7ba9c53f1a2534e71d533ed220e6bc
Thomas Clark <thomas.c.clark.27@dartmouth.edu>
2025-02-26 14:11:40 -0500

    added devtools to index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import thoughtSlice from './thought-slice';
 import passportSlice from './passport-slice';
 
-const useStore = create((set, get) => ({
+const useStore = create(devtools((set, get) => ({
   thoughtSlice: thoughtSlice(set, get),
   passportSlice: passportSlice(set, get),
-}));
+})));
 
 export default useStore;
