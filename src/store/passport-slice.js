4780d3e14d6d50f85082b7c3d60587513ca65412
Thomas Clark <thomas.c.clark.27@dartmouth.edu>
2025-03-10 12:26:33 -0400

    fixed endpoint with correct path

--- src/store/passport-slice.js
+++ src/store/passport-slice.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import { toast } from 'react-toastify';
 
-const ROOT_URL = 'https://project-api-lost-and-found-9lyg.onrender.com/api';
+const ROOT_URL = 'https://project-api-lost-and-found-9lyg.onrender.com/api/countries/unlocked';
 
 export default function PassportSlice(set, get) {
   // NEED TO FETCH UNLOCKED COUNTRIES BY USERID? This will require a change to country routing.
